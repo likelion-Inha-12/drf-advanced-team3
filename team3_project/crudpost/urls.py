0c50a28d404af688d4b4d9dec25d4552ef6b7b7d
SeaHee <lollol0617@gmail.com>
2024-05-08 13:30:29 +0900

    edit README.md func 1,2,7,8 / modify urls.py

--- team3_project/crudpost/urls.py
+++ team3_project/crudpost/urls.py
@@ -3,9 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('post/assign/', views.create_assignment),
-    path('post/submission', views.create_submission),
-    path('post/<str:part>/', views.get_assignment_part),
-    path('post/<str:tag>/', views.get_assignment_tag),
+    path('assign/', views.create_assignment),
+    path('submission/<int:assignment_id>/', views.create_submission),
+    path('part/<str:part>/', views.get_assignment_part),
+    path('tag/<str:tag>/', views.get_assignment_tag),
     path('post/<int:pk>/', views.assignmentAPIView.as_view())
 ]
