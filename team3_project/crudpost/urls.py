dc8d0ffdce12b4f6828da8a47fc4ed8311c05dbf
SeaHee <lollol0617@gmail.com>
2024-05-08 14:18:38 +0900

    오타수정

--- team3_project/crudpost/urls.py
+++ team3_project/crudpost/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('submission/<int:assignment_id>/', views.create_submission),
     path('part/<str:part>/', views.get_assignment_part),
     path('tag/<str:tag>/', views.get_assignment_tag),
-    path('post/<int:pk>/', views.assignmentAPIView.as_view())
+    path('post/<int:pk>/', views.assignmentAPIView.as_view()),
     #api4
     path('assign/view/<int:pk>/',views.assignmentAPIView.as_view(), name="view"),
     #api5
